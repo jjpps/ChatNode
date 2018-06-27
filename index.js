abdcb2d92962cc15964c7b39490736a32e282d4a
aescjpps <joao.policastro@aesc.org.br>
2018-06-27 15:01:00 -0300

    adicionando bootstrap

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+//var bo = require('bootstrap');
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  console.log('alguem se conectou')
 
-  //emit message to all front-end clients
-  io.emit('wlc', 'Bem vindo');
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
-    //socket.broadcast.emit(msg);
-    //console.log(msg);
-  });
-});
 http.listen(3000, function () {
-  console.log('listening on *:3000');
+  console.log('estou no server :3000');
 });
 
 
