b35f02b9c7889d9451d794c2e3c7ba422f91e270
jjpps <joaopedro-ps@hotmail.com>
2018-06-27 22:55:37 -0300

    pagina de login com redirect

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var bo = require('bootstrap');
+var nomeUser = "";
+var $ = require("jquery");
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html;charset=utf8');
+  if (nomeUser == "") {
+    res.sendFile(__dirname + '/index.html');
+  } else { 
+    res.sendFile(__dirname + '/Chat.html');
+  }
+
 });
 
+io.on('connection', function (socket) {
+  socket.on('user', function (data) {
+    if (data != null) {
+      nomeUser = data;
+      console.log(nomeUser);
+    } else {
+      console.log('vazio');
+    }
+  });
+});
 
 http.listen(3000, function () {
   console.log('estou no server :3000');
